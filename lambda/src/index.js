9ef380a787ade291d1a7451fae68f28a834d5e49
NikB <nikb@mac.com>
2015-09-21 00:45:32 -0500

    Adds intent for previous track Adds intent for next track with random responses Adds intent for current track with random responses

--- lambda/src/index.js
+++ lambda/src/index.js
@@ -9,6 +9,19 @@ var EchoSonos = function () {
     AlexaSkill.call(this, options.appid);
 };
 
+var SKIP_RESPONSES = [
+    generateResponseTemplate("Skipping $currentArtist $currentTitle", true, false),
+    generateResponseTemplate("Not a fan of $currentArtist?  Alright, we can skip them.", false, false),
+    generateResponseTemplate("Don't like $currentTitle?  Skipping it.", false, false),
+    generateResponseTemplate("No $currentArtist for you?  How about $nextArtist $nextTitle instead?", false, true)
+];
+
+var STATE_RESPONSES = [
+    generateResponseTemplate("This is $currentArtist $currentTitle", true, false),
+    generateResponseTemplate("We're listening to $currentTitle by $currentArtist", false, false),
+    generateResponseTemplate("$currentTitle by $currentArtist", false, false)
+];
+
 EchoSonos.prototype = Object.create(AlexaSkill.prototype);
 EchoSonos.prototype.constructor = EchoSonos;
 
@@ -33,6 +46,20 @@ EchoSonos.prototype.intentHandlers = {
         console.log("VolumeUpIntent received");
         options.path = '/groupVolume/+10';
         httpreq(options, response, "OK");
+    },
+    NextTrackIntent: function (intent, session, response) {
+        console.log("NextTrackIntent received");
+        skipHttpreq(options, response);
+    },
+    PreviousTrackIntent: function (intent, session, response) {
+        console.log("PreviousTrackIntent received");
+        options.path = '/previous';
+        httpreq(options, response, "OK");
+    },
+    WhatsPlayingIntent: function (intent, session, response) {
+        console.log("WhatsPlayingIntent received");
+        options.path = '/state';
+        stateHttpreq(options, response);
     }
 };
 
@@ -44,10 +71,86 @@ exports.handler = function (event, context) {
 };
 
 function httpreq(options, alexaResponse, responseText) {
-  console.log("Trying http request with responseText " + responseText);
+  //console.log("Trying http request with responseText " + responseText);
   http.request(options, function(httpResponse) {
-    console.log(httpResponse.body);
-    if (responseText)
+    //console.log(httpResponse);
+    if (responseText) {
         alexaResponse.tell(responseText);
+    }
   }).end();
+}
+
+function stateHttpreq(options, alexaResponse) {
+    http.request(options, function(httpResponse) {
+        var body = '';
+        httpResponse.on('data', function(data) {
+            body += data;
+        });
+        httpResponse.on('end', function() {
+            var currentState = JSON.parse(body);
+            console.log("response: " + body)
+            if (currentState.currentTrack) {
+                alexaResponse.tell(generateRandomResponse(STATE_RESPONSES, currentState));
+            }
+        });
+  }).end();
+}
+
+function skipHttpreq(options, alexaResponse) {
+    options.path = "/state";
+    http.request(options, function(httpResponse) {
+        var body = '';
+        httpResponse.on('data', function(data) {
+            body += data;
+        });
+        httpResponse.on('end', function() {
+            var currentState = JSON.parse(body);
+            console.log("response1: " + body)
+            options.path = "/next";
+            http.request(options, function(httpResponse2) {
+                console.log("response2: " + httpResponse2)
+                if (currentState.currentTrack) {
+                    alexaResponse.tell(generateRandomResponse(SKIP_RESPONSES, currentState));
+                }
+            }).end();
+        });
+  }).end();
+}
+
+function generateRandomResponse(responseTemplates, currentState) {
+    var index = parseInt(Math.random() * responseTemplates.length, 10);
+    return generateResponse(responseTemplates[index], currentState);
+}
+
+function generateResponse(responseTemplate, currentState) {
+    var currentArtist = currentState.currentTrack.artist;
+    if (responseTemplate.possessiveCurrentArtist) {
+        currentArtist = makePossessive(currentArtist);
+    }
+    var nextArtist = currentState.nextTrack.artist;
+    if (responseTemplate.possessiveNextArtist) {
+        nextArtist = makePossessive(nextArtist);
+    }
+    var response = responseTemplate.template.
+        replace("$currentArtist", currentArtist).
+        replace("$currentTitle", currentState.currentTrack.title).
+        replace("$nextArtist", nextArtist).
+        replace("$nextTitle", currentState.nextTrack.title);
+    console.log("Generated response: " + response);
+    return response;
+}
+
+function makePossessive(name) {
+    if (name.toLowerCase().charAt(name.length - 1) === 's') {
+        return name + "'";
+    }
+    return name + "'s";
+}
+
+function generateResponseTemplate(template, possessiveCurrentArtist, possessiveNextArtist) {
+    return {
+        template: template,
+        possessiveCurrentArtist: possessiveCurrentArtist,
+        possessiveNextArtist: possessiveNextArtist
+    }
 }
