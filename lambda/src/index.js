16ae6a354e15afddf5001e4178d9d6994ff5eb61
Ryan Graciano <ryan@ryangraciano.com>
2015-12-20 15:44:10 -0800

    Fixed busted responses from Echo

--- lambda/src/index.js
+++ lambda/src/index.js
@@ -25,74 +25,80 @@ EchoSonos.prototype.intentHandlers = {
     PlayIntent: function (intent, session, response) {
         console.log("PlayIntent received");
         options.path = '/preset/'+encodeURIComponent(intent.slots.Preset.value);
-        httpreq(options, function() {});
-        response.tell("");
+        httpreq(options, function() {
+            response.tell("OK");
+        });
     },
 
     PlaylistIntent: function (intent, session, response) {
         console.log("PlaylistIntent received");
         options.path = '/' + encodeURIComponent(intent.slots.Room.value) + '/playlist/' + encodeURIComponent(intent.slots.Preset.value);
-        httpreq(options, function() {});
-        response.tell("");
+        httpreq(options, function() {
+            response.tell("OK");
+        });
     },
 
     FavoriteIntent: function (intent, session, response) {
         console.log("FavoriteIntent received");
         options.path = '/' + encodeURIComponent(intent.slots.Room.value) + '/favorite/' + encodeURIComponent(intent.slots.Preset.value);
-        httpreq(options, function() {});
-        response.tell("");
+        httpreq(options, function() {
+            response.tell("OK");
+        });
     },
 
     ResumeIntent: function (intent, session, response) {
         console.log("ResumeIntent received");
         options.path = '/resumeall';
-        httpreq(options, function() {});
-        response.tell("");
+        httpreq(options, function() {
+            response.tell("OK");
+        });
     },
 
     PauseIntent: function (intent, session, response) {
         console.log("PauseIntent received");
         options.path = '/pauseall';
-        httpreq(options, function() {});
-        response.tell("");
+        httpreq(options, function() {
+            response.tell("OK");
+        });
     },
 
     VolumeDownIntent: function (intent, session, response) {
         console.log("VolumeDownIntent received");
         options.path = '/' + encodeURIComponent(intent.slots.Room.value) + '/volume/-10';
-        httpreq(options, function() {});
-        response.tell("");
+        httpreq(options, function() {
+            response.tell("OK");
+        });
     },
 
     VolumeUpIntent: function (intent, session, response) {
         console.log("VolumeUpIntent received");
         options.path = '/' + encodeURIComponent(intent.slots.Room.value) + '/volume/+10';
-        httpreq(options, function() {});
-        response.tell("");
+        httpreq(options, function() {
+            response.tell("OK");
+        });
     },
 
     SetVolumeIntent: function (intent, session, response) {
         console.log("SetVolumeIntent received");
         options.path = '/'  + encodeURIComponent(intent.slots.Room.value) + '/volume/' + encodeURIComponent(intent.slots.Percent.value);
-        httpreq(options, function() {});
-        response.tell("");
+        httpreq(options, function() {
+            response.tell("OK");
+        });
     },
 
     NextTrackIntent: function (intent, session, response) {
         console.log("NextTrackIntent received");
 
         actOnCoordinator(options, '/next', intent.slots.Room.value,  function (responseBodyJson) {
-            // do nothing on response; hearing the track change is ample confirmation
+            response.tell("OK");
         });
-        response.tell("");
     },
 
     PreviousTrackIntent: function (intent, session, response) {
         console.log("PreviousTrackIntent received");
         actOnCoordinator(options, '/previous', intent.slots.Room.value,  function (responseBodyJson) {
-            // do nothing on response; hearing the track change is ample confirmation
+            response.tell("OK");
         });
-        response.tell("");
     },
 
     WhatsPlayingIntent: function (intent, session, response) {
@@ -100,6 +106,8 @@ EchoSonos.prototype.intentHandlers = {
         options.path = '/' + encodeURIComponent(intent.slots.Room.value) + '/state';
 
         httpreq(options, function (responseJson) {
+            responseJson = JSON.parse(responseJson);
+            
             var randResponse = Math.floor(Math.random() * STATE_RESPONSES.length);
             
             var responseText = STATE_RESPONSES[randResponse].replace("$currentTitle", responseJson.currentTrack.title).replace("$currentArtist", responseJson.currentTrack.artist);
@@ -111,19 +119,20 @@ EchoSonos.prototype.intentHandlers = {
     MuteIntent: function (intent, session, response) {
         console.log("MuteIntent received");
         options.path = '/' + encodeURIComponent(intent.slots.Room.value) + '/mute';
-        httpreq(options, function() {});
-        response.tell("");
+        httpreq(options, function() {
+            response.tell("OK");
+        });
     },
 
     UnmuteIntent: function (intent, session, response) {
         console.log("UnmuteIntent received");
         options.path = '/' + encodeURIComponent(intent.slots.Room.value) + '/unmute';
-        httpreq(options, function() {});
-        response.tell("");
+        httpreq(options, function() {
+            response.tell("OK");
+        });
     },
 }
 
-// HTTP call intended for node-sonos-http-api. Parses response & returns as JSON object
 function httpreq(options, responseCallback) {
   http.request(options, function(httpResponse) {
       var body = '';
@@ -133,7 +142,7 @@ function httpreq(options, responseCallback) {
       });
       
       httpResponse.on('end', function() {
-          responseCallback(JSON.parse(body));
+          responseCallback(body);
       });
   }).end();
 }
