1af0f34ab625f152d41df45dd255cdeb1d3f1095
Ryan Graciano <ryan@ryangraciano.com>
2016-02-14 10:10:55 -0800

    Added example options for HTTP basic auth

--- lambda/src/options.example.js
+++ lambda/src/options.example.js
@@ -1,7 +1,15 @@
+// If you setup basic auth in node-sonos-http-api's settings.json, change the username
+// and password here.  Otherwise, just leave this alone and it will work without auth.
+var auth = new Buffer("YOUR_USERNAME" + ":" + "YOUR_PASSWORD").toString("base64");
+
 var options = {
   appid: "amzn1.echo-sdk-ams.app.ENTER_YOUR_APP_ID_FOR_ECHO_HERE",
   host: "host_for_sonos_api",
-  port: "5005"
+  port: "5005",
+  headers: {
+      'Authorization': 'Basic ' + auth,
+      'Content-Type': 'application/json'
+  }
 };
 
 module.exports = options;
